9e58d1239d9110d8d5d71848bccbcee64420c0e2
André Luis Tosato da Cruz <andreluistosatoo@gmail.com>
2019-01-28 23:15:46 -0200

    First ReactDOM and JSX practice

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// Objective: Fill in the boilerplate React code required to render an
+// unordered list (<ul>) to the page. The list should contain 3 list items
+// (<li>) with anything in them you want.
+
+// HINTS:
+// import the libraries you need first
+// use one of the libraries to render some JSX to the page
+
+ReactDOM.render(
+    <ul>
+        <li>Eat</li>
+        <li>Train</li>
+        <li>Sleep</li>
+    </ul>,
+    document.getElementById('root')
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
