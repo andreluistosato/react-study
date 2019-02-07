38606530f7fe01e70b989809a15b3bac65abb525
André Luis Tosato da Cruz <andreluistosatoo@gmail.com>
2019-02-06 22:55:46 -0200

    Functional components and Parent/Child Components

---
+++ src/components/GeneralStuff/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyInfo from './MyInfo';
+
+function MainContent() {
+    return (
+        <main>
+            <MyInfo />
+        </main>
+    )
+}
+
+export default MainContent
---
+++ src/components/GeneralStuff/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>André Luis Tosato da Cruz</h1>
+            <p>This is a paragraph about me...</p>
+            <ul>
+                <li>Eat</li>
+                <li>Train</li>
+                <li>Sleep</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo
