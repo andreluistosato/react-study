365a4146126b892f596f01304b6f8b4d9de642e4
André Luis Tosato da Cruz <andreluistosatoo@gmail.com>
2019-05-22 22:28:59 -0300

    Inline Styles with the Style Property

--- src/components/general/MyInfo.js
+++ src/components/general/MyInfo.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 function MyInfo() {
-    const time = new Date().getHours();
+    const hours = new Date().getHours();
+    let timeOfDay;
+    const styles = {
+        color: "blue", 
+        textAlign: "center",
+        //fontSize: 20 <- this is automatically use px
+        //fontSize: "20px" <- if i need to specify the unit, quotes are necessary
+    };
+
+    if (hours < 12) {
+        timeOfDay = "morning";
+        styles.color = "orange";
+      } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon";
+        styles.color = "blue";
+      } else {
+        timeOfDay = "night";
+        styles.color = "purple";
+      }
 
     return (
         <div>
@@ -12,9 +30,12 @@ function MyInfo() {
                 <li>Train</li>
                 <li>Sleep</li>
             </ul>
-            <h2><b>It's about {time}h!</b></h2>
+            <h1 style={styles}>Good {timeOfDay}!</h1>
+            <h2 style={{color: "red", textAlign: "left"}}><b>It's about {hours}h!</b></h2>
+            <h2 style={styles}><b>It's about {hours}h!</b></h2>
+            <h2 style={{color: "green", textAlign: "right"}}><b>It's about {hours}h!</b></h2>
         </div>
     )
 }
 
-export default MyInfo
+export default MyInfo;
